93472928fd2e2413242e4b429c65dbe2e58fbe56
ajeer333 <ajeer333@gmail.com>
2023-01-29 14:03:21 +0100

    commit

--- netflix-ui-start/a.js
+++ netflix-ui-start/a.js
@@ -1,48 +1,104 @@
-const url = "https://striveschool-api.herokuapp.com/api/movies"
+
+const url=" https://striveschool-api.herokuapp.com/api/movies"
+const options={
+    headers: {
+    "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2M2NhNTdlM2U3MzczODAwMTUzNzQ2YjMiLCJpYXQiOjE2NzQ5MTQ5MDksImV4cCI6MTY3NjEyNDUwOX0.ErrhpOV2vLIxZGyxFjkI0z1wXjiYSYTPJAovrerz6Xs"     }
+   }
 
 
-window.onload = async () => {
-  await getMovies()
-}
 
-const getMovies = async () => {
-  try {
-    const options={
-        headers: new Headers({
-            "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2M2NhNTdlM2U3MzczODAwMTUzNzQ2YjMiLCJpYXQiOjE2NzQyMDUxNTUsImV4cCI6MTY3NTQxNDc1NX0.jJ1EPinzZ6vr8H6n7Lmekw1F6MoVJ5_W7lrrXhhsMJ0",
-        }),
-    }
-    const res = await fetch(url,options)
-    const movies = await res.json() 
-    renderMovies(movies)
-  } catch (error) {
-    console.log(error)
+
+const renderSections = async (genres) => {
+  try{
+
+    let container = document.getElementById("fetch")
+      genres.forEach(async (genre) => {
+        container.innerHTML += 
+        
+        `<h4 class = "text-white mb-4 mt-4">${genre}</h4>
+        <div id="${genre}" class="row row-cols-2  row-cols-md-4">
+        </div>`;
+        let res= await fetch(url+`/${genre}`,options)
+        let moviesofGenre = await res.json();
+        console.log(moviesofGenre)
+        let row = document.getElementById(genre);
+        moviesofGenre.forEach((movie) => {
+           row.innerHTML += `<div class="px-1 mb-1 mb-lg-0">
+           <div class="movie-card">
+             <img class="w-100" src="${movie.imageUrl}" alt="" />
+             <div class="infos-container w-100 mt-2">
+               <div class="d-flex mb-3 mt-4 align-items-center">
+                 <i class="bi bi-play-circle  px-1"></i>
+                 <i class="bi bi-hand-thumbs-up px-1 "></i>
+                 <div id="alertvisible"><i class="bi bi-plus-circle px-1"></i></div> 
+                 <div class="alert alert-dark addtolist" role="alert">
+                   Add to your list
+                 </div>
+                 <h6 class="pt-2 pl-1">${movie.name}</h6>
+                 <i class="bi bi-chevron-down ml-auto "></i>
+   
+               </div>
+               <div class="deeet d-flex justify-content-around align-items-start">
+               <h6 >98% Match</h6>
+               <h6 class="pr-1">12</h6>
+             
+               <h6 class="pb-0">HD</h6>
+             </div>
+   
+             
+               <div class="card-movie-footer d-flex align-items-center">
+                 <span class="mr-2">${movie.category}</span>
+                 
+                 
+               </div>
+             </div>
+           </div>
+         </div>`;
+         });
+       });
+
   }
+  catch(err){
+      console.log(err)
+  }
+      
+  };
+
+const getMovies=async()=>{
+    try{
+        let res= await fetch(url,options)
+        let data=await res.json()
+        console.log(data)
+        renderSections(data)
+    }
+    catch(err){
+        console.log(err)
+    }
 }
 
-const renderMovies = (arrayOfMovies) => {
-    const movieNode=document.getElementById("movieGoHere")
-  arrayOfMovies.forEach((singleMovie) => {
-    // const { name, description, imageUrl, category,_id} = singleMovie
-    // console.log(singleMovie)
-    movieNode.innerHTML += `<div class="col-6 col-md-3 px-1 mb-1 mb-lg-0">
-    <div class="list-group-item w-50">
-    <div class='row align-items-center justify-content-between p-3'>
-      <div class='col col-6'>
-      <h3> ${singleMovie.name} </h3>
-            <p> ${singleMovie.description} </p>
-            <span class="badge badge-pill badge-info">${singleMovie.category}</span>
-      </div> 
-      <img src=${singleMovie.imageUrl}/>
-
-      </div>
-      </div>`
-  })
-}
+   window.onload=()=>{
+    getMovies();
+   }
 
 
 
 
 
-  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
--- netflix-ui-start/backoffice.js
+++ netflix-ui-start/backoffice.js
@@ -1,46 +1,144 @@
-const url="https://striveschool-api.herokuapp.com/api/movies"
-
-
-const postMovie=async (movieToPost)=>{
-try{
-    movieToPost.preventDefault();
-
-    const name=document.querySelector("#movieName").value
-    const description=document.querySelector("#movieDescription").value
-    const category=document.querySelector("#movieCategory").value
-    const imageUrl=document.querySelector("#movieImage").value
-    const newMovie={name,description,category,imageUrl}
-    console.log(newMovie)
-
-
-    
-        const options = {
-                 method: "POST", 
-                body: JSON.stringify(newMovie), 
-                   headers: new Headers({
-                   "Content-Type": "application/json", 
-                   "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2M2NhNTdlM2U3MzczODAwMTUzNzQ2YjMiLCJpYXQiOjE2NzQyMDUxNTUsImV4cCI6MTY3NTQxNDc1NX0.jJ1EPinzZ6vr8H6n7Lmekw1F6MoVJ5_W7lrrXhhsMJ0"
-                }),
-               }
-
-    let res=await fetch(url,options)
-    
-
-    if(res.ok){
-        alert("movie added")
+const url=" https://striveschool-api.herokuapp.com/api/movies"
+ let allMovies = [];
+ let movieToEdit;
+const options={
+    headers: {
+        "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2M2NhNTdlM2U3MzczODAwMTUzNzQ2YjMiLCJpYXQiOjE2NzQ5MTQ5MDksImV4cCI6MTY3NjEyNDUwOX0.ErrhpOV2vLIxZGyxFjkI0z1wXjiYSYTPJAovrerz6Xs"     }
     }
-    console.log(res.json())
-
+const deleteMovie=async(id)=>{
+    let res=await fetch(url+"/"+id, {
+            method:"DELETE",
+            headers: {
+                "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2M2NhNTdlM2U3MzczODAwMTUzNzQ2YjMiLCJpYXQiOjE2NzQ5MTQ5MDksImV4cCI6MTY3NjEyNDUwOX0.ErrhpOV2vLIxZGyxFjkI0z1wXjiYSYTPJAovrerz6Xs"
+                }
+        })
+    let movies = await res.json();
+    onLoadActions();
 
 }
-catch(error){
-    console.log(error )
+
+
+const editMovieBtn = async (id) => {
+  let movie = allMovies.find(
+    (item) => item._id === id
+  );
+ document.getElementById("name").value=movie.name
+ document.getElementById("description").value=movie.description
+ document.getElementById("category").value=movie.category
+ document.getElementById("imageUrl").value=movie.imageUrl
+
+
+   movieToEdit = id;
+
+};
+
+
+const editMovies=async(submitEvent)=>{
+    try{
+        submitEvent.preventDefault()
+        let name=document.getElementById("name").value
+        let description=document.getElementById("description").value
+        let category=document.getElementById("category").value
+        let imageUrl=document.getElementById("imageUrl").value
+        
+        let editedMovie={name,description,category,imageUrl}
+
+        let options={
+                method:"PUT",
+                body: JSON.stringify(editedMovie),
+            headers:new Headers({
+                "Content-Type": "application/json",
+                "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2M2NhNTdlM2U3MzczODAwMTUzNzQ2YjMiLCJpYXQiOjE2NzQ5MTQ5MDksImV4cCI6MTY3NjEyNDUwOX0.ErrhpOV2vLIxZGyxFjkI0z1wXjiYSYTPJAovrerz6Xs"
+            })
+        }
+        let res=await fetch(url+"/"+movieToEdit,options)
+        let data=await res.json()
+        onLoadActions();
+    }
+    catch(err){
+        console.log(err)
+    }
+    
 }
+
+ const postMovies=async(submitEvent)=>{
+    try{
+        let alertmsg=document.querySelector('.alert-success')
+        submitEvent.preventDefault()
+                let name=document.getElementById("name").value
+                let description=document.getElementById("description").value
+                let category=document.getElementById("category").value
+                let imageUrl=document.getElementById("imageUrl").value
+                let newMovie={name,description,category,imageUrl}
+           
+        
+        
+                let options={
+                        method:"POST",
+                        body:JSON.stringify(newMovie),
+                    headers: new Headers({
+                              "Content-Type": "application/json", 
+                              "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2M2NhNTdlM2U3MzczODAwMTUzNzQ2YjMiLCJpYXQiOjE2NzQ5MTQ5MDksImV4cCI6MTY3NjEyNDUwOX0.ErrhpOV2vLIxZGyxFjkI0z1wXjiYSYTPJAovrerz6Xs"
+                            }),
+                }
+                let res=await fetch(url,options)
+                let data=await res.json()
+                console.log(data)
+                onLoadActions();
+                
+                alertmsg.classList.replace("d-none","d-block")
+                 setTimeout(() => {
+                    alertmsg.classList.replace("d-block","d-none")
+                 }, 3000);
+    }
+    catch(err){
+        let alertmsg=document.querySelector('.alert-danger')
+        alertmsg.classList.replace("d-none","d-block")
+        setTimeout(() => {
+           alertmsg.classList.replace("d-block","d-none")
+        }, 3000);
+    }
+ }
+
+
+const movieList = async (genres) => {
+        let node = document.getElementById("heregoesgenres");
+        node.innerHTML=""
+        genres.forEach(async (genre) => {
+          node.innerHTML += `<h2 class="text-danger mb-3">${genre}</h2>
+          <ul id=${genre} class="list-group list-group-horizontal mb-5 mt-5" style="color:black">
+          </ul>`
+          let res= await fetch(url+`/${genre}`,options)
+          let movies = await res.json();
+          console.log(movies)
+          let list =document.getElementById(genre)
+          movies.forEach((movie) => {  
+          allMovies.push(movie);
+          list.innerHTML+=`
+          <li class="list-group-item">${movie.name}</li>
+          <li class="list-group-item"><img height="40" width="40" src=${movie.imageUrl}</li>
+          <li class="list-group-item"><button type="button" class="btn btn-danger" onclick = "deleteMovie('${movie._id}')">Delete</button></li>
+          <li class="list-group-item"><button type="button" class="btn btn-primary" onclick = "editMovieBtn('${movie._id}')">Edit</button></li>
+          "\n"
+          `
+          });
+
+         });
+      
+      };    
+const onLoadActions = async () => {
+    allMovies=[]
+    try {
+      let res = await fetch(url,options)
+      let genres = await res.json();
+      console.log(genres)
+      movieList(genres);
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+window.onload =()=>{
+    onLoadActions();
+ 
 }
-
-
-
-
-
-
-
